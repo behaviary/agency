63f02e8bb9e9c46e37406e25f609624999814205
Tyler Smith <tylsmith@gmail.com>
2017-06-05 11:37:07 -0700

    page scaffold

--- app/controllers/frontend_controller.rb
+++ app/controllers/frontend_controller.rb
@@ -1,4 +1,16 @@
 class FrontendController < ApplicationController
     def home
     end
+
+    def portfolio
+    end
+
+    def testimonials
+    end
+
+    def team
+    end
+
+    def contact
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'frontend#home'
   get 'home', to: 'frontend#home'
+  get 'portfolio', to: 'frontend#portfolio'
+  get 'testimonials', to: 'frontend#testimonials'
+  get 'team', to: 'frontend#team'
+  get 'contact', to: 'frontend#contact'
 end
